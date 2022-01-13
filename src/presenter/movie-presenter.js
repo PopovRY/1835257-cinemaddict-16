7260c35eec250b336cf38fe657496244f3c54dc2
PopovRY <rupopov91@gmail.com>
2022-01-13 17:26:36 +1000

    movie-presenter WIP

--- src/presenter/movie-presenter.js
+++ src/presenter/movie-presenter.js
@@ -3,7 +3,7 @@ import FilmPopupView from '../view/film-popup-view';
 import {COMMENTS_ARRAY} from '../mock/structures';
 import {remove, render, RenderPosition} from '../utils/render';
 import FilmsListContainer from '../view/films-list-container';
-import {onEscKeyDown} from '../utils/utils';
+import {onEscKeyDown, replace} from '../utils/utils';
 
 
 export default class MoviePresenter {
@@ -21,14 +21,35 @@ export default class MoviePresenter {
   init = (film) => {
     this.#film = film;
 
+    const prevFilmCardComponent = this.#filmCardComponent;
+    const prevPopupComponent = this.#popupComponent;
+
     this.#filmCardComponent = new FilmCardView(film);
     this.#popupComponent = new FilmPopupView(film, this.#comments);
 
     this.#filmCardComponent.setFilmCardClickHandler(this.#handleFilmCardClick);
     this.#popupComponent.setPopupClickHandler(this.#handlePopupClick);
 
-    render(this.#filmContainer, this.#filmCardComponent, RenderPosition.BEFOREEND);
 
+    if (prevFilmCardComponent === null || prevPopupComponent === null) {
+      render(this.#filmContainer, this.#filmCardComponent, RenderPosition.BEFOREEND);
+    }
+
+    if (this.#filmContainer.element.contains(prevFilmCardComponent)) {
+      replace(this.#filmCardComponent, prevFilmCardComponent);
+    }
+
+    if (this.#filmContainer.element.contains(prevPopupComponent)) {
+      replace(this.#popupComponent, prevPopupComponent);
+    }
+
+    remove(prevFilmCardComponent);
+    remove(prevPopupComponent);
+  }
+
+  destroy = () => {
+    remove(this.#filmCardComponent);
+    remove(this.#popupComponent);
   }
 
   #openPopup = () => {
